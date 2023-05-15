978957c07ef6137faa200d75b5dd68084810ce27
cristiberzescu <berzescu.c31@gmail.com>
2023-05-15 23:57:50 +0300

    first commit

--- server.js
+++ server.js
@@ -12,38 +12,60 @@ con.connect(function (error) {
   if (error) {
     console.log(error);
   } else {
-    var participantsTable =
-      "CREATE TABLE IF NOT EXISTS Participants(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL)";
+    // var participantsTable =
+    //   "CREATE TABLE IF NOT EXISTS Participants(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50) NOT NULL)";
 
-    con.query(participantsTable, (err, res) => {
+    // con.query(participantsTable, (err, res) => {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     console.log("participants connected");
+    //   }
+    // });
+
+    // var meetingsTable =
+    //   "CREATE TABLE IF NOT EXISTS Meetings(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL, id_Host INT UNSIGNED NOT NULL)";
+
+    // con.query(meetingsTable, (err, res) => {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     console.log("meetings connected");
+    //   }
+    // });
+
+    var idParticipantsAndMeetings =
+      "CREATE TABLE IF NOT EXISTS idParticipantsAndMeetings(idMeet INT, idParticipant INT, PRIMARY KEY(idMeet, idParticipant))";
+
+    con.query(idParticipantsAndMeetings, (err, res) => {
       if (err) {
         console.log(err);
       } else {
-        console.log("participants connected");
-      }
-    });
-
-    var meetingsTable =
-      "CREATE TABLE IF NOT EXISTS Meetings(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL, id_Host INT UNSIGNED NOT NULL)";
-
-    con.query(meetingsTable, (err, res) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("meetings connected");
+        console.log("ids connected");
       }
     });
 
     con.query(
-      "CREATE VIEW meetingHost AS SELECT Meetings.id_Host, Participants.name FROM Meetings INNER JOIN participants WHERE Meetings.id_Host = Participants.id",
+      "INSERT INTO idParticipantsAndMeetings (idMeet, idParticipant) select Meetings.id, Participants.id from Meetings, Participants where Meetings.id_Host = participants.id",
       (err, res) => {
         if (err) {
           console.log(err);
         } else {
-          console.log("view meeting host id");
+          console.log("meet ids added");
         }
       }
     );
+
+    // con.query(
+    //   "INSERT INTO idParticipantsAndMeetings (1,1)",
+    //   (err, res) => {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //       console.log("meet ids added");
+    //     }
+    //   }
+    // );
 
     // con.query(
     //   "INSERT INTO Participants (name) VALUES ('Alin'),('Mihai'),('Marius')",
@@ -63,6 +85,17 @@ con.connect(function (error) {
     //       console.log(err);
     //     } else {
     //       console.log("meetings added");
+    //     }
+    //   }
+    // );
+
+    // con.query(
+    //   "CREATE VIEW meetingHost AS SELECT Meetings.id_Host, Participants.name FROM Meetings INNER JOIN participants WHERE Meetings.id_Host = Participants.id",
+    //   (err, res) => {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //       console.log("view meeting host id");
     //     }
     //   }
     // );
